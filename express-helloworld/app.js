610b6be801b3cca2242d07bcab0bdc8dc39a60c9
AlbertoMorillo <amorilloavila25@hotmail.com>
2020-05-25 12:47:07 -0400

    V2

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -9,6 +9,10 @@ app.get('/Mars', function (req, res) {
   res.send('Hello Mars!\n');
 });
 
+app.get('/v2', function (req, res) {
+  res.send('Hello, there"\'"s a version 2 !\n');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
